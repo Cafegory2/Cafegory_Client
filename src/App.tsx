38f82a792a01cebb43f779ce953fd9397fc42036
jinsupark4255 <jin04007@naver.com>
2024-07-07 17:43:35 +0900

    Chore: 상태관리 라이브러리, axiios 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyle from "./styles/GlobalStyle";
+
 function App() {
-  return <></>;
+  const queryClient = new QueryClient();
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
