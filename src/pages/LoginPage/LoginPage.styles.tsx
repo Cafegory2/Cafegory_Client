070626db54a6f8409891497e3d6ce129b5755570
이무성 <79708688+dlantjdgkgk@users.noreply.github.com>
2024-08-17 17:28:37 +0900

    Revert "Feat/16 hamburger button"

--- src/pages/LoginPage/LoginPage.styles.tsx
+++ src/pages/LoginPage/LoginPage.styles.tsx
@@ -1,32 +1,4 @@
 //로그인 페이지 스타일 여기서 처리
 import styled from "styled-components";
-import { Color } from "../../constants";
 
-export const Layout = styled.div`
-  width: 100%;
-  height: calc(100% - 110px); //전체에서 헤더 높이 뺀 길이
-  color: black;
-`;
-
-export const SectionTop = styled.section`
-  width: 100%;
-  height: 60%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const SectionBottom = styled.section`
-  width: 100%;
-  height: 40%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-export const Bar = styled.div`
-  width: 169px;
-  height: 1px;
-  margin: 5px 0px 16px 0px;
-  background-color: ${Color.Gray};
-`;
+export const LoginPageLayout = styled.div``;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,24 +1,11 @@
-import * as Style from "./LoginPage.styles";
-import Logo from "../../assets/Logo.svg";
-import { Text } from "../../common/Text";
-import { Color } from "../../constants";
-import KakaoLogin from "../../components/login/KakaoLogin";
-import GoogleLogin from "../../components/login/GoogleLogin";
-import { LoginLayout } from "../../styles/Layout.styles";
+//로그인 페이지에 필요한 컴포넌트들을 import해서 여기서 합침
+import { LoginPageLayout } from "./LoginPage.styles";
 
 const LoginPage = () => {
   return (
-    <LoginLayout>
-      <Style.SectionTop>
-        <img src={Logo} alt="Logo" style={{ width: "40%" }} />
-      </Style.SectionTop>
-      <Style.SectionBottom>
-        <Text $color={Color.Gray}>SNS 계정으로 로그인</Text>
-        <Style.Bar />
-        <KakaoLogin />
-        <GoogleLogin />
-      </Style.SectionBottom>
-    </LoginLayout>
+    <LoginPageLayout>
+      여기다 로직 컴포넌트 + 스타일링 컴포넌트 합치기
+    </LoginPageLayout>
   );
 };
 
